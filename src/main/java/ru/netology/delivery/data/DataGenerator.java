ca8a72791383097b3fab333810c49dc0477aba46
Alyona Yedinorozhik <misterrpandaa@gmail.com>
2025-08-12 17:31:10 +0300

    Initial commit

---
+++ src/main/java/ru/netology/delivery/data/DataGenerator.java
@@ -0,0 +1,54 @@
+package ru.netology.delivery.data;
+
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    public static String generateDate(int shift) {
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String generateCity() {
+        String[] cities = new String[]{"Барнаул", "Чита", "Хабаровск", "Кострома", "Нижний Новгород", "Ростов-на-Дону",
+                "Ханты-Мансийск", "Москва", "Черкесск", "Краснодар"};
+        return cities[new Random().nextInt(cities.length)];
+    }
+
+    public static String generateName(String local) {
+        Faker faker = new Faker(new Locale(local));
+        return faker.name().fullName();
+    }
+
+    public static String generatePhone(String local) {
+        Faker faker = new Faker(new Locale(local));
+        return faker.phoneNumber().phoneNumber();
+    }
+
+    public static class Registration {
+        static Faker faker;
+
+        private Registration() {
+        }
+
+        public static UserInfo generateUser(String locale) {
+            faker = new Faker(new Locale("ru"));
+            return new UserInfo(generateCity(), generateName(locale), generatePhone(locale));
+        }
+    }
+
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
+    }
+}
---
+++ src/test/java/ru/netology/delivery/data/GeneratorTest.java
@@ -0,0 +1,48 @@
+package ru.netology.delivery.data;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.util.Locale;
+
+import static com.codeborne.selenide.Selenide.$;
+import static java.lang.Character.getName;
+import static ru.netology.delivery.data.DataGenerator.*;
+import static ru.netology.delivery.data.DataGenerator.Registration.faker;
+
+public class GeneratorTest {
+
+    @BeforeEach
+    void setup() {
+        Selenide.open("http://localhost:9999/");
+    }
+
+    @Test
+    void positiveEntryForm() {
+        DataGenerator.UserInfo validUser = DataGenerator.Registration.generateUser("ru");
+        String firstDate = generateDate(3);
+        String newDate = generateDate(6);
+
+        $("[data-test-id='city'] input").setValue(generateCity());
+        $("[data-test-id='date'] input").press(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.DELETE)).setValue(firstDate);
+        $("[data-test-id='name'] input").setValue(validUser.getName());
+        $("[data-test-id='phone'] input").setValue(validUser.getPhone());
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $("[data-test-id='success-notification'] .notification__content")
+                .shouldHave(Condition.text("Встреча успешно запланирована на " + firstDate))
+                .shouldBe(Condition.visible);
+        $("[data-test-id='date'] input").press(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.DELETE)).setValue(newDate);
+        $("button.button").click();
+        $("button.button").click();
+        $("[data-test-id='success-notification'] .notification__content")
+                .shouldHave(Condition.text("Встреча успешно запланирована на " + newDate))
+                .shouldBe(Condition.visible);
+
+
+    }
+}
